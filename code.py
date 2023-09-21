6d3821b5f0aad8c3fb873b3ca2c4fc345b15f267
Doodpy <garbonzo.chickpea@gmail.com>
2023-09-20 21:36:21 -0500

    Firmware..........??????

---
+++ code.py
@@ -0,0 +1,24 @@
+print("Starting")
+
+import board
+
+from kmk.kmk_keyboard import KMKKeyboard
+from kmk.keys import KC
+from kmk.scanners import DiodeOrientation
+
+keyboard = KMKKeyboard()
+
+keyboard.col_pins = (board.GP0, board.GP1, board.GP2, board.GP3, board.GP4, board.GP5, board.GP6, board.GP7, board.GP8, board.GP9, board.GP10, board.GP11, board.GP12, board.GP13,)
+keyboard.row_pins = (board.GP14, board.GP15, board.GP16, board.GP17, board.GP18)
+keyboard.diode_orientation = DiodeOrientation.COL2ROW
+
+keyboard.keymap = [
+    [KC.GRAVE, KC.N1, KC.N2, KC.N3, KC.N4, KC.N5, KC.N6, KC.N7, KC.N8, KC.N9, KC.N0, KC.MINUS, KC.EQUAL, KC.BSPACE,
+    KC.TAB, KC.Q, KC.W, KC.E, KC.R, KC.T, KC.Y, KC.U, KC.I, KC.O, KC.P, KC.LBRACKET, KC.RBRACKET, KC.BSLASH,
+    KC.CAPSLOCK, KC.A, KC.S, KC.D, KC.F, KC.G, KC.H, KC.J, KC.K, KC.L, KC.SCOLON, KC.QUOTE, KC.ENTER,
+    KC.LSHIFT, KC.Z, KC.X, KC.C, KC.V, KC.B, KC.N, KC.M, KC.COMMA, KC.DOT, KC.SLASH, KC.RSHIFT,
+    KC.LCTRL, KC.LGUI, KC.LALT, KC.SPACE, KC.RALT, KC.RGUI, KC.WINMENU, KC.RCTRL]
+]
+
+if __name__ == '__main__':
+    keyboard.go()
